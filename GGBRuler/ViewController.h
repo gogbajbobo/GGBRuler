fa27eacfb3bebac3f569cd595738d8212c37a169
gogbajbobo <grigoriev.maxim@gmail.com>
2017-06-12 16:50:45 +0300

    Initial Commit

---
+++ GGBRuler/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GGBRuler
+//
+//  Created by Maxim Grigoriev on 12/06/2017.
+//  Copyright © 2017 Maxim Grigoriev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
